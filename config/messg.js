49c83f9d34f3b0decf447588ee11de431f00879f
zegu <18573608129@163.com>
2020-11-25 20:33:24 +0800

    feat:Add annotation and Beautify the output

--- config/messg.js
+++ config/messg.js
@@ -1,9 +1,10 @@
+const Colors = require("colors");
 const welcomeInfo =
-  "欢迎加入node聊天室请选择操作\n（1）.注册\n（2）.登陆\n (3) .退出 \n键入 ':help' 获取更多帮助信息\n";
-const signAndLoginInfo = "请输入用户名和密码用户名和密码用空格隔开\n   ";
-const signErro = "输入的用户名或密码无效 或者已经存在同名用户\n";
-const userEnd = "断开连接\n";
-const loginSuccseInfo = "注册成功\n";
+  "欢迎加入node聊天室请选择操作\n（1）.注册\n（2）.登陆\n (3) .退出 \n键入 ':help' 获取更多帮助信息\n网络不是法外之地！！！！\n";
+const signAndLoginInfo = "请输入用户名和密码用户名和密码用空格隔开\n   ".blue;
+const signErro = "输入的用户名或密码无效 或者已经存在同名用户\n".red;
+const userEnd = "断开连接\n".red;
+const loginSuccseInfo = "注册成功\n".green;
 const helpMesg = `
 :q         quit                     退出客户端
 :help      help                     帮助中心
@@ -11,9 +12,9 @@ const helpMesg = `
 :lu        list user                列出在线用户     
 `;
 
-const quiteMesg = "\n您即将退出客户端\n";
-const notLoginMesg = "\n您未登录\n";
-
+const quiteMesg = "\n您即将退出客户端\n".red;
+const notLoginMesg = "\n您未登录\n".red;
+const commendErr = "指令错误\n".red;
 module.exports = {
   signAndLoginInfo,
   signErro,
@@ -23,4 +24,5 @@ module.exports = {
   quiteMesg,
   notLoginMesg,
   welcomeInfo,
+  commendErr,
 };
--- server.js
+++ server.js
@@ -10,9 +10,41 @@ const {
   quiteMesg,
   notLoginMesg,
   welcomeInfo,
+  commendErr,
 } = require("./config/messg");
+const { write } = require("fs");
+
+const SIGN = "1";
+const LOGIN = "2";
+const QUITE = "3";
 
 const userList = new Map();
+
+//获取在线用户
+const getOnlineUser = () => {
+  let onlineUser = [];
+  for (u of userList) {
+    if (u[1].conn) {
+      onlineUser.push(u[0]);
+    }
+  }
+  return onlineUser;
+};
+
+//发送在线用户信息
+const sendUserListInfo = (conn) => {
+  let onlineUser = getOnlineUser();
+  let userListMesg = `您当前所在房间共有${userList.size}人 其中在线 ${onlineUser.length}人\n:\n`;
+  onlineUser.forEach((e) => {
+    userListMesg += `${e}   `;
+  });
+  conn.write(userListMesg + "\n");
+};
+
+const addBoult = (conn) => {
+  conn.write("\n> ");
+};
+
 const server = net.createServer((conn) => {
   conn.setEncoding("utf-8");
   let user = null;
@@ -25,7 +57,12 @@ const server = net.createServer((conn) => {
   if (user) {
     console.log(user.userName);
   }
+
   conn.write(welcomeInfo);
+  //显示用户在线情况
+  sendUserListInfo(conn);
+  addBoult(conn);
+
   conn.on("data", (data) => {
     data = data.trim();
 
@@ -37,6 +74,7 @@ const server = net.createServer((conn) => {
         case "h":
         case "help": {
           conn.write(helpMesg);
+          addBoult(conn);
           break;
         }
         //退出客户端
@@ -52,26 +90,19 @@ const server = net.createServer((conn) => {
           if (!user) {
             conn.write(notLoginMesg);
             conn.write(welcomeInfo);
+            addBoult(conn);
             break;
           }
           conn.write(logoutMesg);
           user = null;
           conn.write(welcomeInfo);
+          addBoult(conn);
         }
         //列出在线用户
         case "lu":
         case "list-user": {
-          let onlineUser = [];
-          for (u of userList) {
-            if (u[1].conn) {
-              onlineUser.push(u[0]);
-            }
-          }
-          let userListMesg = `您当前所在房间共有${userList.size}人 其中在线 ${onlineUser.length}人\n:\n`;
-          onlineUser.forEach((e) => {
-            userListMesg += `${e}   `;
-          });
-          conn.write(userListMesg + "\n");
+          sendUserListInfo(conn);
+          addBoult(conn);
           break;
         }
       }
@@ -82,17 +113,30 @@ const server = net.createServer((conn) => {
       //用户空 进入登陆注册环节
       //强制将用户输入用户名和密码的步骤改为一步
 
-      if (data === "1") {
+      //注册
+      if (data === SIGN) {
         conn.write(signAndLoginInfo);
-        LOGINFLAG = true;
-      } else if (data === "2") {
-        conn.write(signAndLoginInfo);
+        addBoult(conn);
+
+        //注册放行
         SIGNFLAG = true;
-      } else if (data === "3") {
+      }
+      // 登陆
+      else if (data === LOGIN) {
+        conn.write(signAndLoginInfo);
+        addBoult(conn);
+
+        // 登陆放行
+        LOGINFLAG = true;
+      }
+      //退出
+      else if (data === QUITE) {
         conn.write(quiteMesg);
         conn.destroy();
         return;
-      } else if (LOGINFLAG) {
+      }
+      //注册逻辑
+      else if (SIGNFLAG) {
         let userInfo = data.split(" ");
         const vali = (userInfo.length != 2) | userList.has(userInfo[0]);
 
@@ -100,6 +144,7 @@ const server = net.createServer((conn) => {
           conn.write(signErro);
           setTimeout(() => {
             conn.write(signAndLoginInfo);
+            addBoult(conn);
           }, 500);
           return;
         }
@@ -107,9 +152,12 @@ const server = net.createServer((conn) => {
         user = new User(userInfo[0], userInfo[1], conn);
         userList.set(user.userName, user);
         conn.write(loginSuccseInfo);
+        addBoult(conn);
 
-        LOGINFLAG = false;
-      } else if (SIGNFLAG) {
+        SIGNFLAG = false;
+      }
+      //登陆逻辑
+      else if (LOGINFLAG) {
         let userInfo = data.split(" ");
         const valiRuller = userInfo.length != 2;
 
@@ -117,6 +165,7 @@ const server = net.createServer((conn) => {
           conn.write(signErro);
           setTimeout(() => {
             conn.write(signAndLoginInfo);
+            addBoult(conn);
           }, 500);
           return;
         }
@@ -126,6 +175,7 @@ const server = net.createServer((conn) => {
           conn.write("用户不存在\n");
           setTimeout(() => {
             conn.write(signAndLoginInfo);
+            addBoult(conn);
           }, 500);
           return;
         }
@@ -134,16 +184,22 @@ const server = net.createServer((conn) => {
         let result = user.login(userInfo[1]);
         if (result) {
           conn.write("登陆成功\n");
+          addBoult(conn);
           if (user.conn) {
             user.conn.close();
           }
           user.setConn(conn);
-          SIGNFLAG = false;
+          LOGINFLAG = false;
           return;
         }
         conn.write("登陆失败，帐号或密码错误\n");
+        addBoult(conn);
 
         user = null;
+      } else {
+        conn.write(commendErr);
+        conn.write(welcomeInfo);
+        addBoult(conn);
       }
       return;
     }
@@ -154,18 +210,20 @@ const server = net.createServer((conn) => {
     if (matchResult) {
       let u = userList.get(matchResult[1]);
       u.sendMsg(user.userName + ":" + matchResult[2] + "\n");
+      addBoult(conn);
       return;
     }
 
     //用户存在 转发消息
     for (u of userList) {
       if (u[1].userName !== user.userName) {
-        console.log(u[1].userName);
         let mesg = `${user.userName}:${data}\n`;
         u[1].sendMsg(mesg);
+        addBoult(conn);
+      } else {
+        addBoult(conn);
       }
     }
-    console.log(user.userName + "   " + data);
   });
 
   conn.on("end", () => {
